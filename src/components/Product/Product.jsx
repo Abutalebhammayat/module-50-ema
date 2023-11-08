7ef30719e4e76a001df595aa46811b258587b2e7
Abutalebhammayat <iamaabutaleb461@gmail.com>
2023-11-09 01:03:05 +0600

    add d to another item

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import './Product.css'
 const Product = (props) => {
     const { name, img, price, seller, ratings } = props.product;
-    console.log(props.product)
+    // console.log(props.product);
+    const handlarAddToCart=props.handlarAddToCart;
+    // onClick={()=>handlarAddToCart()}
+
     return (
         <div className='product'>
             <img src={img} alt="" />
             <div className='cart-data'>
                 <span className='font-big'>{name}</span><br />
                   <span className='font-medium'>Price: $ {price}</span><br /><br />
-                  <span className='font-small'>Manufacturer: {seller}</span><br />
-                  <span className='font-small'>Rating: {ratings} start</span>
+                  <span className='font-small'>Seller Name: {seller}</span><br />
+                  <span className='font-small'>Rating: {ratings}</span>
             </div>
-            <button className='shopping-card-btn'>Add to Cart<i class="fa-solid fa-cart-shopping"></i></button>
+            <button onClick={()=>handlarAddToCart(props.product)} className='shopping-card-btn'>Add to Cart<i class="fa-solid fa-cart-shopping"></i></button>
         </div>
     );
 };
